4369a46fc321ba4146c25fc0fce468d19fa27ecb
Beka Sturua <beqasturua3@gmail.com>
2022-09-05 20:36:58 +0400

    feat: use useCallBack and search is work

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -8,30 +8,15 @@ const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
   useEffect(() => {
-    fetch(
-      "https://react-hoo-d1fe1-default-rtdb.firebaseio.com/ingredients.json"
-    )
-      .then((Response) => Response.json())
-      .then((responseData) => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
-          });
-        }
-        setUserIngredients(loadedIngredients);
-      });
-  }, []);
-
-  useEffect(() => {
     console.log("Render", userIngredients);
   }, [userIngredients]);
 
-  const filteredIngredientsHandler = (filteredIngredients) => {
-    setUserIngredients(filteredIngredients);
-  };
+  const filteredIngredientsHandler = useCallback(
+    (filteredIngredients) => {
+      setUserIngredients(filteredIngredients);
+    },
+    [setUserIngredients]
+  );
 
   const addIngredientHandler = (ingredient) => {
     fetch(
